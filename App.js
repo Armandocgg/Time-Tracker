1f80a9807f19ef4138926508226ac8c51d1d231f
Armando Gúzman <armando@ideamosweb.com>
2019-01-21 19:21:34 -0500

    Timers Array..

--- App.js
+++ App.js
@@ -1,40 +1,69 @@
 import React from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
+import  { v4 } from 'uuid'
 import Header from '@/components/Header'
 import ToggleableTimerForm from '@/containers/ToggleableTimerForm'
 import EditableTimer from '@/containers/EditableTimer'
 
 export default class App extends React.Component {
+  // {} Objeto
+  // [] Array
+  state = {
+    timers: [
+      {
+        id: v4(),
+        title: 'Create Tinder',
+        project: 'Bootcamp',
+        elapsed: '8986300',
+        isRunning: false
+      },
+      {
+        id: v4(),
+        title: 'Create Face',
+        project: 'Bootcamp',
+        elapsed: '4548300',
+        isRunning: false
+      },
+      {
+        id: v4(),
+        title: 'Create Inst',
+        project: 'Bootcamp',
+        elapsed: '3624300',
+        isRunning: false
+      },{
+        id: v4(),
+        title: 'Create You',
+        project: 'Bootcamp',
+        elapsed: '6666300',
+        isRunning: false
+      }
+    ]
+  }
+
   render() {
+    const { timers } = this.state
     return (
-      <View style={styles.container}>
+      <View style={ styles.container }>
         <Header title="Time Tracker"></Header>
         <ToggleableTimerForm 
-          onFormSubmit={() => null} //this.handleCreateFormSubmit
+          onFormSubmit={ () => null} //this.handleCreateFormSubmit
         />
-        <ScrollView style={styles.container}>
-          <EditableTimer
-            id="1"
-            title="Create timer app"
-            project="Bootcamp"
-            elapsed="8986300"
-            isRunning 
-            onFormSubmit={ () => null } //this.handleFormSubmit
-            onRemovePress={() => null } //this.handleRemovePress
-            onStartPress={() => null } //this.toggleTimer
-            onStopPress={() => null } //this.toggleTimer
-          />
-          <EditableTimer
-            id="2"
-            title="Mow the lawn"
-            project="House Chores"
-            elapsed="8986300"
-            isRunning={false}
-            onFormSubmit={ () => null } //this.handleFormSubmit
-            onRemovePress={() => null } //this.handleRemovePress
-            onStartPress={() => null } //this.toggleTimer
-            onStopPress={() => null } //this.toggleTimer
-          />    
+        <ScrollView style={ styles.container }>
+          { timers.map(({ id, title, project, elapsed, isRunning }) => (
+            <EditableTimer
+              key={ id }
+              id={ id }
+              title={ title }
+              project={ project }
+              elapsed={ elapsed }
+              isRunning={ isRunning }
+              onFormSubmit={ () => null } //this.handleFormSubmit
+              onRemovePress={ () => null } //this.handleRemovePress
+              onStartPress={ () => null } //this.toggleTimer
+              onStopPress={ () => null } //this.toggleTimer
+            />
+            ))
+          }
         </ScrollView>
       </View>
     )
